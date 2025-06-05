62441bf669c406c2df6a675e186957430788caae
Bohdan Olenin <oleninbr@gmail.com>
2025-06-05 10:26:19 +0000

    Лабораторна 4: адмінка категорій

--- app/Models/BlogCategory.php
+++ app/Models/BlogCategory.php
@@ -2,23 +2,19 @@
 
 namespace App\Models;
 
-//use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BlogCategory extends Model
 {
+    use SoftDeletes;
     use HasFactory;
-
-    /*protected $fillable = [
-        'name',
-        'slug',
-        'description',
-        'image',
-        'status',
-    ];
-
-    public function blogs()
-    {
-        return $this->hasMany(Blog::class);
-    }*/
+    protected $fillable
+        = [
+            'title',
+            'slug',
+            'parent_id',
+            'description',
+        ];
 }
