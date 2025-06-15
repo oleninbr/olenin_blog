434f1fe5b853b59e86b7fc349435c8ff0e40f0a3
Bohdan Olenin <oleninbr@gmail.com>
2025-06-15 15:43:19 +0000

    Лабораторна 8. Керування статтями блога

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -2,12 +2,33 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
 
 class BlogPost extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'category_id',
+        'excerpt',
+        'content_raw',
+        'content_html',
+        'is_published',
+        'published_at',
+        'user_id',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(BlogCategory::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
