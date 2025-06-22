3d59bb273f5f6ce12b7fd623ab891a814bab47ed
Bohdan Olenin <oleninbr@gmail.com>
2025-06-22 17:58:13 +0000

    Лабораторна 15. Vue, API

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\Blog\PostController;
+
+Route::get('blog/posts', [PostController::class, 'index']);
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
